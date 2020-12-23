d19369f463d4a431c0b2c16daad1aaed6f0eb02e
shaikhsajid1111 <shaikhsajid11112000@gmail.com>
2020-12-23 12:28:13 +0530

    code readibility

--- facebook_page_scraper/driver_initialization.py
+++ facebook_page_scraper/driver_initialization.py
@@ -1,12 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 try:
     from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options as ChromeOptions
+    #to add capabilities for chrome and firefox, import their Options with different aliases
+    from selenium.webdriver.chrome.options import Options as ChromeOptions   
     from selenium.webdriver.firefox.options import Options as FirefoxOptions
+    #import webdriver for downloading respective driver for the browser
     from webdriver_manager.chrome import ChromeDriverManager
     from webdriver_manager.firefox import GeckoDriverManager
-except ModuleNotFoundError:
-    print("Please download dependencies")
+except Exception as ex:
+    print(ex)
 
 
 class Initializer:
@@ -15,10 +17,11 @@ class Initializer:
         self.browser_name = browser_name
 
     def set_properties(self,browser_option):
-        browser_option.add_argument('--no-sandbox')
-#        browser_option.add_argument('--headless')
+        """adds capabilities to the driver"""
+        #browser_option.add_argument('--headless')   #runs browser in headless mode
+        browser_option.add_argument('--no-sandbox') 
+
         browser_option.add_argument("--disable-dev-shm-usage")
-        browser_option.add_argument("disable-infobars")
         browser_option.add_argument('--ignore-certificate-errors')
         browser_option.add_argument('--disable-gpu')
         browser_option.add_argument('--log-level=3')
@@ -27,13 +30,20 @@ class Initializer:
         return browser_option
 
     def set_driver_for_browser(self,browser_name):
+        """expects browser name and returns a driver instance"""
+        #if browser is suppose to be chrome 
         if browser_name == "chrome":
             browser_option = ChromeOptions()
+            #automatically installs chromedriver and initialize it and returns the instance
             return webdriver.Chrome(executable_path=ChromeDriverManager().install(),options=self.set_properties(browser_option))
         elif browser_name == "firefox":
             browser_option = FirefoxOptions()
+            #automatically installs geckodriver and initialize it and returns the instance
             return webdriver.Firefox(executable_path=GeckoDriverManager().install(),options=self.set_properties(browser_option))
-    
+        else:
+            #if browser_name is not chrome neither firefox than raise an exception 
+            raise Exception("Browser not supported!")
     def init(self):
+        """returns driver instance"""
         driver = self.set_driver_for_browser(self.browser_name)
         return driver
