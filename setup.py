e087c9fa19637349bc388717c6dae7e56cf6bc19
shaikhsajid1111 <shaikhsajid11112000@gmail.com>
2021-03-16 10:28:22 +0530

    updated to version 0.1.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.MD","r") as file:
 
 setuptools.setup(
     name = "facebook_page_scraper",
-    version = "0.1.7",
+    version = "0.1.8",
     author = "Sajid Shaikh",
     author_email = "shaikhsajid3732@gmail.com",
     description = "Python package to scrap facebook's pages front end with no limitations",
